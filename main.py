3d8f497d457376a215ec1f96bba0e286e50d1dfc
Annette29 <annette.irungu@jkuat.ac.ke>
2024-07-26 12:44:31 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from generate_masks import process_files
+from compare_files import compare_files
+
+# Define your paths here
+svs_folder = "/your/original svs images/folder"
+geojson_folder = "/your/geojson data/folder"
+output_folder = "/store binary masks here/folder"
+
+# Process SVS files and corresponding GeoJSON files
+process_files(svs_folder, geojson_folder, output_folder)
+
+# Move SVS files based on generated masks
+move_files(svs_folder, output_folder, destination_folder)
