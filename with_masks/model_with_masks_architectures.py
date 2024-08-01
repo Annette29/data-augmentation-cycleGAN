15e96bc1c6148bdaa572f27ed674d171d0ad8c0a
Annette29 <annette.irungu@jkuat.ac.ke>
2024-08-01 14:19:16 +0900

    Update model_with_masks_architectures.py

--- with_masks/model_with_masks_architectures.py
+++ with_masks/model_with_masks_architectures.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import itertools
-import os
 from torchvision import models
 from torchvision.models.resnet import ResNet34_Weights
 import torch.nn.functional as F
